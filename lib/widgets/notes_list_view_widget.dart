33954abb0a58034c34e3abca07f21eef7bcef4e8
mostafa <mustafa.bahr12323@gmail.com>
2023-01-11 11:23:16 +0200

    create  noteItem listview

---
+++ lib/widgets/notes_list_view_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'custom_note_item.dart';
+class NotesListView extends StatelessWidget {
+  const NotesListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context,index){
+          return const Padding(
+            padding:   EdgeInsets.symmetric(vertical: 5),
+            child:   CustomNoteItem(),
+          );
+        },
+      ),);
+  }
+}
--- lib/widgets/notes_view_body_widget.dart
+++ lib/widgets/notes_view_body_widget.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_app_bar.dart';
 import 'custom_note_item.dart';
+import 'notes_list_view_widget.dart';
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
 
@@ -12,14 +13,15 @@ class NotesViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: const [
-          SizedBox(height: 50,),
-          CustomAppBar(),
-          SizedBox(height: 20,),
-          CustomNoteItem(),
+            SizedBox(height: 50,),
+            CustomAppBar(),
+          NotesListView(),
         ],
       ),
     );
   }
 }
+
+
 
 
