ffda89f9eee0e93c97eaa39853a667b705919eff
mostafa <mustafa.bahr12323@gmail.com>
2023-01-12 22:20:05 +0200

    note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,14 @@
+class NoteModel
+{
+  final String noteTitle;
+  final String noteContent;
+  final String date;
+  final int color;
+
+  NoteModel({
+    required this.color,
+    required this.noteContent,
+    required this.noteTitle,
+    required this.date,
+});
+}
