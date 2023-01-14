3eeccade5a9ec9e1833c4335f0517a1b72c3501f
mostafa <mustafa.bahr12323@gmail.com>
2023-01-14 13:23:52 +0200

    delete item from list

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -37,7 +37,9 @@ class CustomNoteItem extends StatelessWidget {
                 ),),
               ),
               trailing: IconButton(
-                onPressed: (){},
+                onPressed: (){
+                  noteModel.delete();
+                },
                 icon:const Icon(FontAwesomeIcons.trash,color: Colors.black87,size: 24,),
               ),
             ),
